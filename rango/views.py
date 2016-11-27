4032ea9a3e44a6d9b7c64416378a71dd343456cd
Ali Soliman <ali.soliman95@gmail.com>
2016-11-27 03:31:51 +0200

    4. Templates and Views

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    context_dict = {'boldmessage': "Crucnhy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'myname': "Ali"}
+    return render(request, 'rango/about.html',context=context_dict)
