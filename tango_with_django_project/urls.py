1386fc4b4f68b10d3de24444cb8477ae9c832fc8
Ali Soliman <ali.soliman95@gmail.com>
2016-11-30 05:13:50 +0200

    Add Forms and use HTML Templates

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -19,8 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 
-from rango import views
-
 urlpatterns = [
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
